e4185d06425076666cd2c1930ff4032580fe1f4f
salkicky <salkicky@gmail.com>
2012-03-18 17:00:59 +0900

    2nd change for 18_sample.rb

--- 18/18_sample.rb
+++ 18/18_sample.rb
@@ -1,4 +1,20 @@
 # encoding:shift_jis
 
+# 
+# Command Line Option
+#
+
+# "-d"
 puts "===== (1) ====="
 puts "debug mode" if $DEBUG
+
+
+# "-s"
+# ex. > ruby -s thisscript.rb -foo -bar 
+
+puts "$foo: #{$foo}"
+puts "$bar: #{$bar}"
+puts "$baz: #{$baz}"
+
+
+

7937f6e429006a87fc5d9e4a1bdbed8bb8ea50b8
salkicky <salkicky@gmail.com>
2012-05-02 23:07:08 +0900

    add Practical

---
+++ 18/aaa.rb
@@ -0,0 +1,20 @@
+# encoding: shift_jis
+
+
+class WarningTest
+  def initialize
+    @test = "test."
+  end
+
+  def test
+    print @tset,"\n"
+  end
+end
+
+sample_test = WarningTest.new
+sample_test.test
+
+Line one
+Line two
+Line three
+Line four
